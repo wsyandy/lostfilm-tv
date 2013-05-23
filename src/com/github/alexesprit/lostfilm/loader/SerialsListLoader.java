18d28e197a4390d49089a780957fa5d084e85895
alexesprit <alex.esprit@gmail.com>
2013-05-23 13:19:36 +0400

    Add load and parse timecounters

--- src/com/github/alexesprit/lostfilm/loader/SerialsListLoader.java
+++ src/com/github/alexesprit/lostfilm/loader/SerialsListLoader.java
@@ -1,5 +1,6 @@
 package com.github.alexesprit.lostfilm.loader;
 
+import android.util.Log;
 import com.github.alexesprit.lostfilm.Util;
 import com.github.alexesprit.lostfilm.item.SerialItem;
 
@@ -11,8 +12,13 @@ public class SerialsListLoader {
     private static final String SERIAL_PATTERN = "<a href=\"(.+?)\" class=\"bb_a\">(.+?)<br><span>";
 
     public ArrayList<SerialItem> getSerialsList() {
+        long t0, t1;
+        t0 = System.currentTimeMillis();
         String content = Util.getURLContent("http://www.lostfilm.tv/");
+        t1 = System.currentTimeMillis();
+        Log.i("LostFilm", "[serials] load time: " + (t1 - t0) + " sec.");
         if (null != content) {
+            t0 = System.currentTimeMillis();
             ArrayList<SerialItem> serials = new ArrayList<SerialItem>();
             Matcher m = Pattern.compile(SERIAL_PATTERN).matcher(content);
             while (m.find()) {
@@ -21,6 +27,8 @@ public class SerialsListLoader {
                 item.descURL = Util.getFullURL(m.group(1));
                 serials.add(item);
             }
+            t1 = System.currentTimeMillis();
+            Log.i("LostFilm", "[serials] parse time: " + (t1 - t0) + " sec.");
             return serials;
         }
         return null;
